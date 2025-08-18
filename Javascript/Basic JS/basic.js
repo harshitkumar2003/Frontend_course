497cf96c126c87b20b950ad5c677e0be144daabf
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-08-18 23:24:13 +0530

    `Added JavaScript basics, including console.log, variables, and variable reassignment examples.`

--- Javascript/Basic JS/basic.js
+++ Javascript/Basic JS/basic.js
@@ -1,7 +1,26 @@
-// TODO Starting javacript series form thia file basic understanding of JavaScript
+// TODO Javacript series 
 // This is a simple JavaScript file to demonstrate basic syntax and functionality
 
 // TODO what is console.log in JavaScript?
 // console.log is a function used to print messages to the web console, which is useful for debugging purposes.
 console.log("Hello, World!");
 // It outputs the string "Hello, World!" to the console.
+
+// -----------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------------
+
+// TODO Variables in JavaScript
+// Variables are used to store data values. In JavaScript, you can declare variables using var, let, or const.
+const accountId = 1011; // const is used for constants that won't change
+let acountEmail = "harshit@gmail.com"; // let is used for variables that can change
+var accountPassword = "12345"; // var is an older way to declare variables, but still valid 
+accountCity = "Meerut"; // If you don't declare a variable with var, let, or const, it becomes a global variable
+// -----------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------------
+// ! Reassigning variables
+// accountId = 2022; // This will throw an error because accountId is a constant and cannot be reassigned
+accountEmail = "HCHC@.com"; // This is valid because acountEmail is declared with let and can be reassigned
+accountPassword = "67890"; // This is valid because accountPassword is declared with var and can be reassigned
+accountCity = "Delhi"; // This is valid because accountCity is declared with let and can be reassigned
+console.log(accountId); // Outputs: 1011 
+console.table([accountId, acountEmail, accountPassword, accountCity]); // Outputs the variables in a table format
