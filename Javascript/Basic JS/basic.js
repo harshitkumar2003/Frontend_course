5f0f1cd92c1dedeeb681bda5280f60a0f4063123
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-08-18 22:58:56 +0530

    creatig js file for starting javascript

---
+++ Javascript/Basic JS/basic.js
@@ -0,0 +1,7 @@
+// TODO Starting javacript form thia file basic understanding of JavaScript
+// This is a simple JavaScript file to demonstrate basic syntax and functionality
+
+// TODO what is console.log in JavaScript?
+// console.log is a function used to print messages to the web console, which is useful for debugging purposes.
+console.log("Hello, World!");
+// It outputs the string "Hello, World!" to the console.
