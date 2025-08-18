c0e937264da05183bb604e80832c1a306dad2408
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-08-18 23:45:25 +0530

    `Update basic.js: fix variable declarations, add comments, and correct variable names`

--- Javascript/Basic JS/basic.js
+++ Javascript/Basic JS/basic.js
@@ -12,15 +12,17 @@ console.log("Hello, World!");
 // TODO Variables in JavaScript
 // Variables are used to store data values. In JavaScript, you can declare variables using var, let, or const.
 const accountId = 1011; // const is used for constants that won't change
-let acountEmail = "harshit@gmail.com"; // let is used for variables that can change
+let accountEmail = "harshit@gmail.com"; // let is used for variables that can change
 var accountPassword = "12345"; // var is an older way to declare variables, but still valid 
+//! Prefer not to use var in modern JavaScript
 accountCity = "Meerut"; // If you don't declare a variable with var, let, or const, it becomes a global variable
+let accountState; // let can be used to declare a variable that can change later. undefined by default
 // -----------------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------------
-// ! Reassigning variables
+//! Reassigning variables
 // accountId = 2022; // This will throw an error because accountId is a constant and cannot be reassigned
-accountEmail = "HCHC@.com"; // This is valid because acountEmail is declared with let and can be reassigned
+accountEmail = "HCHC@gmail.com"; // This is valid because acountEmail is declared with let and can be reassigned
 accountPassword = "67890"; // This is valid because accountPassword is declared with var and can be reassigned
 accountCity = "Delhi"; // This is valid because accountCity is declared with let and can be reassigned
 console.log(accountId); // Outputs: 1011 
-console.table([accountId, acountEmail, accountPassword, accountCity]); // Outputs the variables in a table format
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]); // Outputs the variables in a table format
