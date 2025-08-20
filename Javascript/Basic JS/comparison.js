2f154c3739d094abee5fafff760b401369320a63
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-08-20 22:46:15 +0530

    Add basic comparison operators examples in comparison.js

---
+++ Javascript/Basic JS/comparison.js
@@ -0,0 +1,7 @@
+// TODO Some basic comparison operators in JavaScript
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
