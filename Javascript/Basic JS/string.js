20a336f3b202791ab65966a76968733f64275468
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-08-24 00:34:55 +0530

    `Removed console.log statements from string.js file`

--- Javascript/Basic JS/string.js
+++ Javascript/Basic JS/string.js
@@ -8,12 +8,19 @@
 
 
 const name = new String(`HARSHIT`);
-console.log(name); // String object
+// console.log(name); //! String object
 
-console.log(name[0]);
+// console.log(name[0]);
 // Accessing characters in a string
-console.log(name.length); // Length of the string
-console.log(name.toLowerCase()); // Convert to uppercase
-console.log(name.toUpperCase()); // Convert to lowercase
-console.log(name.charAt(2)); //
-console.log(name.indexOf('S')); //
+// console.log(name.length); //! Length of the string
+// console.log(name.toLowerCase()); //! Convert to uppercase
+// console.log(name.toUpperCase()); //! Convert to lowercase
+// console.log(name.charAt(2)); //! R
+// console.log(name.indexOf('S')); //! 3
+
+// const newname = name.substring(1,5);
+// console.log(newname) // ARSH
+
+const anothername = name.slice(3,2);
+console.log(anothername); // 
+
