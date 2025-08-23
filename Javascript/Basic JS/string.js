040d9bd23b6c9b7b23649b8c3728377b61763650
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-08-24 00:22:23 +0530

    `Added explanation of primitive and non-primitive data types in JavaScript, including examples of stack and heap memory usage.`

---
+++ Javascript/Basic JS/string.js
@@ -0,0 +1,18 @@
+// const name = "John Doe";
+// const age = 30;
+
+// TODO console.log("My name is " + name + " and age is " + age);
+// Using template literals for better readability
+// string interpolation
+// console.log(`My name is ${name} and age is ${age}`);
+
+
+const name = new String(`HARSHIT`);
+console.log(name); // String object
+
+console.log(name[0]);
+// Accessing characters in a string
+console.log(name.length); // Length of the string
+console.log(name.toLowerCase()); // Convert to uppercase
+console.log(name.toUpperCase()); // Convert to lowercase
+console.log(name.charAt(2)); // 
--- Javascript/datatypes-summary.js
+++ Javascript/datatypes-summary.js
@@ -46,3 +46,34 @@ console.log(typeof myFunction); // Outputs: function
 console.log(typeof heros); // Outputs: object
 console.log(typeof myObj); // Outputs : object
 console.log(typeof anotherId); // Outputs: symbol
+
+//!----------------------------------------------------------------------------------------------------------------
+//! Stack (PRIMITIVE DATA TYPES):
+// TODO Primitive data types are stored directly in the stack memory. They are immutable and have a fixed size.
+let num1 = 10;
+let num2 = num1; // num2 is a copy of num1
+num1 = 20; // Changing num1 does not affect num2
+console.log(num1); // Outputs: 20
+console.log(num2); // Outputs: 10
+
+let name = "Harshit";
+let anotherName = name; // anotherName is a copy of name
+name = "John"; // Changing name does not affect anotherName
+console.log(name); // Outputs: John
+console.log(anotherName); // Outputs: Harshit
+
+//! Heap (NON-PRIMITIVE DATA TYPES):
+// TODO Non-primitive data types are stored in the heap memory. They are mutable and can change their size.
+let user = {
+    name: "Harshit",
+    age: 21
+};
+
+let anotherUser = user; // anotherUser is a reference to the same object in memory
+user.age = 22; // Changing user affects anotherUser
+
+console.log(user); // Outputs: { name: 'Harshit', age: 22 }
+
+console.log(user.age); // Outputs: 22
+  
+console.log(anotherUser.age); // Outputs: 22
