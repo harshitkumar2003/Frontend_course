04c08b9d7cc9b77bb55ef7f953427ff38545ab59
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-08-25 01:10:30 +0530

    `Added basic date and time operations in JavaScript.`

---
+++ Javascript/Basic JS/date&time.js
@@ -0,0 +1,11 @@
+let myDate = new Date()
+console.log(myDate) //!
+console.log(myDate.toString()) //!
+console.log(myDate.toDateString()) //!
+console.log(myDate.toLocaleString()) //!
+
+// console.log(typeof myDate) //!
+
+let myCreated = new Date(2025, 0, 10)
+console.log(myCreated.toLocaleString())
+console.log(myCreated.toLocaleString())
